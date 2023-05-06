fbe131d8a53ae3c2aa80d2e296bfb97ea39f150b
Tomasz <tomasz@siwinski@o2.pl>
2023-05-06 15:10:03 +0200

    add javascript to nav

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,13 +3,26 @@ const navbarBtnX = document.querySelector(".navbar__x");
 const ulList = document.querySelector(".nav .center ul");
 const btnBars = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-x");
+const liItem = document.querySelector("#nav__item");
+const ulList2 = document.querySelector(".nav__list-second-stage");
+const liItem2 = document.querySelector("#nav__item-second-stage-nav");
+const ulList3 = document.querySelector(".nav__list-third-stage");
 
-const showList = () => {
+const showNav = () => {
 	ulList.classList.toggle("hide");
 	iconX.classList.toggle("hide");
 	btnBars.classList.toggle("hide");
-	rotateArrow();
 };
 
-navbarBtnBars.addEventListener("click", showList);
-navbarBtnX.addEventListener("click", showList);
+const showNav2 = () => {
+	ulList2.classList.toggle("nav--hide");
+};
+
+const showNav3 = () => {
+	ulList3.classList.toggle("nav--hide");
+};
+
+navbarBtnBars.addEventListener("click", showNav);
+navbarBtnX.addEventListener("click", showNav);
+liItem.addEventListener("click", showNav2);
+liItem2.addEventListener("click", showNav3);
