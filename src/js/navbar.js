22844b4585e22115c3e10731d46bd146d57d97e0
Tomasz <tomasz@siwinski@o2.pl>
2023-05-14 15:42:22 +0200

    add javascript

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -5,7 +5,7 @@ const btnBars = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-x");
 const liItem = document.querySelector("#nav__item");
 const ulList2 = document.querySelector(".nav__list-second-stage");
-const liItem2 = document.querySelector("#nav__item-second-stage-nav");
+const liItem2 = document.querySelector("#nav__item-second-stage");
 const ulList3 = document.querySelector(".nav__list-third-stage");
 
 const showNav = () => {
@@ -15,14 +15,30 @@ const showNav = () => {
 };
 
 const showNav2 = () => {
-	ulList2.classList.toggle("nav--hide");
+	if (ulList2.classList.contains("nav--hide")) {
+		ulList2.classList.remove("nav--hide");
+	}
+};
+
+const hideNav2 = () => {
+	ulList2.classList.add("nav--hide");
+	ulList3.classList.add("nav--hide");
 };
 
 const showNav3 = () => {
-	ulList3.classList.toggle("nav--hide");
+	if (ulList3.classList.contains("nav--hide")) {
+		ulList3.classList.remove("nav--hide");
+	}
 };
+const hideNav3 = () => {
+	ulList3.classList.add("nav--hide");
+};
+
 
 navbarBtnBars.addEventListener("click", showNav);
 navbarBtnX.addEventListener("click", showNav);
-liItem.addEventListener("click" || "mouseleave", showNav2);
-liItem2.addEventListener("click", showNav3);
+liItem.addEventListener("mouseenter", showNav2);
+liItem.addEventListener("mouseleave", hideNav2);
+liItem2.addEventListener("mouseenter", showNav3);
+liItem2.addEventListener("mouseleave", hideNav3);
+
